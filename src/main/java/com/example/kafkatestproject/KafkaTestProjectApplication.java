edb537e6728296cfe57355bc5c5c079caa72e0c4
Uka0001 <ukraine0001@gmail.com>
2023-05-12 13:49:42 +0300

    initial commit

---
+++ src/main/java/com/example/kafkatestproject/KafkaTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTestProjectApplication.class, args);
+    }
+
+}
